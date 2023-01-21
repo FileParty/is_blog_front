0486003e9a4adf9c7b7af2cac54db3257634a106
Kim in sool <pdg300@naver.com>
2023-01-21 23:51:28 +0900

    20230121

---
+++ src/Header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <Link href="/"></Link>
+            </div>
+            <div>
+                <h1>김인술 이력서</h1>
+            </div>
+            <div>
+                update 2023.01.03
+            </div>
+        </header>
+    )
+}
